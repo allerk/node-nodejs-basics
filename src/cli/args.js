d4239e424310035a94563fc5c5b10291bd32b3d0
Aleksandr Lerko <allerk@ttu.ee>
2024-01-29 21:53:48 +0200

    feat: implement Command line interface

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+    const argvVariables = process.argv.slice(2);
+    for (let i = 0; i < argvVariables.length; i += 2) {
+        const name = argvVariables[i].slice(2);
+        const value = argvVariables[i + 1];
+
+        if (i === argvVariables.length - 2){
+            process.stdout.write(`${name} is ${value}`);
+        } else {
+            process.stdout.write(`${name} is ${value}, `);
+        }
+    }
 };
 
 parseArgs();
